2f4311e11e073633340ac0057bebc37562e3f1fc
Luis Miguel Castro <luiscastroc11@gmail.com>
2021-11-02 17:08:11 -0500

    v1.0

---
+++ Capas/CDatos/CDProducto.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CEntidades;
+
+
+
+namespace CDatos
+{
+    public class CDProducto
+    {
+        Cconexion oconexion = new Cconexion();
+        SqlCommand ocmd = new SqlCommand();
+
+        public bool Guardar_producto(CEProducto oproducto)
+        {
+ 
+            try 
+            {
+
+                ocmd.CommandType = CommandType.StoredProcedure;
+                ocmd.Connection = oconexion.conectar("BDTienda");
+                ocmd.CommandText = "guardar_producto";
+                ocmd.Parameters.Add("@id_producto", oproducto.Id_producto);
+                ocmd.Parameters.Add("@nombre", oproducto.Nombre);
+                ocmd.Parameters.Add("@cantidad", oproducto.Cantidad);
+                ocmd.Parameters.Add("@precio", oproducto.Precio);
+                ocmd.Parameters.Add("@categoria", oproducto.Categoria);
+                ocmd.Parameters.Add("@activo", oproducto.Activo);
+
+                ocmd.ExecuteNonQuery();
+                return true;
+
+            }
+            catch (Exception err)
+            {
+                throw new Exception(err.Message);
+            }
+        }
+
+        public bool Anular_producto(CEProducto oproducto)
+        {
+            
+            try 
+            {
+
+                ocmd.CommandType = CommandType.StoredProcedure;
+                ocmd.Connection = oconexion.conectar("BDTienda");
+                ocmd.CommandText = "anular_producto";
+                ocmd.Parameters.Add("@id_producto", oproducto.Id_producto);
+                ocmd.ExecuteNonQuery();
+                return true;
+
+            }
+
+
+            catch (Exception err)
+            {
+                throw new Exception(err.Message);
+            }
+        }
+
+        public DataSet Consultar_producto(CEProducto oproducto)
+        {
+            try
+            {
+                ocmd.CommandType = CommandType.StoredProcedure;
+                ocmd.Connection = oconexion.conectar("BDTienda");
+                ocmd.CommandText = "consultar_producto";
+                ocmd.Parameters.Add("@id_producto", oproducto.Id_producto);
+                SqlDataAdapter da = new SqlDataAdapter(ocmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+
+            }
+            catch (Exception err)
+            {
+
+                throw new Exception(err.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ Capas/CEntidades/CEProducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEntidades
+{
+    public class CEProducto
+    {
+        private int id_producto;
+        private String nombre;
+        private int cantidad;
+        private int precio;
+        private String categoria;
+        private byte activo;
+
+        public int Id_producto { get => id_producto; set => id_producto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public byte Activo { get => activo; set => activo = value; }
+    }
+}
